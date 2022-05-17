4682af8e8fe862ac54dc4d7809dbbe52894cfbf8
Dimitar Prisadnikov <i454903@fhict.nl>
2022-05-17 23:44:26 +0300

    finished the front end of the mysneakers component

--- sneakers-pwa-app/src/App.js
+++ sneakers-pwa-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Form from './components/Form';
+import MySneakers from './components/MySneakers';
 
 function App() {
   return (
     <>
-    <Form />
+    <MySneakers />
     </>
   );
 }
--- sneakers-pwa-app/src/components/MySneakers.jsx
+++ sneakers-pwa-app/src/components/MySneakers.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Button } from "bootstrap";
 import { Modal } from "bootstrap";
-
+import {Card, Button, Row} from 'react-bootstrap' 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun, faSnowflake, faDroplet } from '@fortawesome/free-solid-svg-icons'
 
 const baseURL = "https://nameless-shelf-91357.herokuapp.com";
 
@@ -25,12 +26,43 @@ const handleShow = () => setShow(true);
     }
 
     return (
-        <><ul>
-            {sneakers.map(sneaker => <li key={sneaker.id}>{sneaker.sneaker_name}</li>
+        <>
+        <Row className="text-center">
+            <h1 className="mt-4 mb-3">My sneakers</h1>
+            <Button>Add new sneakers</Button>
+        </Row>
+
+
+        <ul className="list-unstyled">
+            {sneakers.map(sneaker => 
+            <li key={sneaker.id}>
+                <Card className="text-center my-3 mx-5">
+                <Card.Body>
+                    <Card.Title>{sneaker.sneaker_name}</Card.Title>
+                    <Card.Text>
+                    {sneaker.color} / {sneaker.material}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                <span>
+                {sneaker.color !== "black" && sneaker.material !== "leather" &&
+                    <FontAwesomeIcon icon={faSun} className="mx-2"/>
+                }
+                {sneaker.rain === true &&
+                    <FontAwesomeIcon icon={faDroplet} className="mx-2"/>
+                }
+                {sneaker.snow === true &&
+                    <FontAwesomeIcon icon={faSnowflake} className="mx-2"/>
+                }
+                {sneaker.color === "black" && sneaker.material === "leather" && sneaker.snow === false && sneaker.rain === false &&
+                    <p className="text-muted">Not suitable for any weather type...</p>
+                }
+                </span>
+                </Card.Footer>
+                </Card>
+            </li>
             )}
         </ul>
-
-        
         </>
     )
 }
