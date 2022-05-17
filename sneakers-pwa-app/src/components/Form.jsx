347019e89cbc571be1acbccaef5752f6b4e85284
Dimitar Prisadnikov <i454903@fhict.nl>
2022-05-18 01:21:53 +0300

    fixed form auto details fetch

--- sneakers-pwa-app/src/components/Form.jsx
+++ sneakers-pwa-app/src/components/Form.jsx
@@ -13,79 +13,50 @@ import {
 } from "react-bootstrap";
 import { useEffect } from "react";
 import axios from "axios";
-import MySneakers from "./MySneakers";
 
-function Form1({ handleModal, showModal }) {
-  const [show1, setShow1] = useState(false);
+const baseURL = "https://nameless-shelf-91357.herokuapp.com";
 
-  const [showAlert, setShowAlert] = useState(false);
+  function Form1({ handleModal, showModal }) {
 
-  const handleCloseAlert = () => setShowAlert(false);
-  const handleShowAlert = () => setShowAlert(true);
+    // todo Plamen: use the apiResponse const to add sneakers. (must)
+    // Regardless of the the details being added automatically (Google Vision API) or manually (user filling text field and checkboxes)
+    // make sure to use this state to accomodate those details.
+    const [sneakersDetails, setSneakerDetails] = useState({ 
+        name: '',
 
-  const handleClose1 = () => setShow1(false);
-  const handleShow1 = () => setShow1(true);
+        // score of the image search between 0 and 1 where 1 means the Google's API is 100% certain that it is the exact same image
+        // todo Plamen: if the score is !=0 display it in a horizontal bar (should)
+        score: -1, 
+        material: '',
+        color: '',
 
-  const baseURL = "https://nameless-shelf-91357.herokuapp.com";
+        // indicates if the selected shoes are suitable for rain/snow
+        //todo Plamen: add checkboxes for those (must)
+        rain: false,
+        snow: false 
+    })
 
-  // todo Plamen: use the apiResponse const to add sneakers. (must)
-  // Regardless of the the details being added automatically (Google Vision API) or manually (user filling text field and checkboxes)
-  // make sure to use this state to accomodate those details.
-  const [sneakersDetails, setSneakerDetails] = useState({
-    name: "",
-
-    // score of the image search between 0 and 1 where 1 means the Google's API is 100% certain that it is the exact same image
-    // todo Plamen: if the score is !=0 display it in a horizontal bar (should)
-    score: -1,
-    material: "",
-    color: "",
-
-    // indicates if the selected shoes are suitable for rain/snow
-    //todo Plamen: add checkboxes for those (must)
-    rain: false,
-    snow: false,
-  });
-
-  function addSneakers() {
-    axios.post(baseURL + "/addsneakers", {
-      name: sneakersDetails.name,
-      material: sneakersDetails.material,
-      color: sneakersDetails.color,
-      rain: sneakersDetails.rain,
-      snow: sneakersDetails.snow,
-    });
-  }
-
-  const setStateFromChild = (data) => {
-    setSneakerDetails({
-      name: data.product.displayName,
-      score: data.score,
-      material: data.product.productLabels[0].value,
-      color: data.product.productLabels[1].value,
-      rain: data.product.productLabels[2].value === "t" ? true : false,
-      snow: data.product.productLabels[2].value === "t" ? true : false,
-    });
-  };
-
-  const clearFields = () => {
-    const noDetails = {
-      product: {
-        name: "",
-        productLabels: ["", "", false, false],
-      },
-    };
-
-    setSneakerDetails(noDetails);
-  };
-
-  React.useEffect(() => {
-    if (sneakersDetails.score < 0.75) {
-      handleShowAlert();
-      clearFields();
-    } else {
-      handleCloseAlert();
+    function addSneakers(){
+      axios.post(baseURL + "/addsneakers", {
+        name: sneakersDetails.name,
+        material: sneakersDetails.material,
+        color: sneakersDetails.color,
+        rain: sneakersDetails.rain,
+        snow: sneakersDetails.snow
+      })
     }
-  });
+
+    const setStateFromChild = (data) => { 
+        setSneakerDetails(
+            {
+              name: data.product.displayName, 
+              score: data.score, 
+              material: data.product.productLabels[0].value,
+              color: data.product.productLabels[1].value,
+              rain: data.product.productLabels[2].value === "t" ? true : false,
+              snow: data.product.productLabels[2].value === "t" ? true : false
+            })
+     }
 
   return (
     <>
@@ -93,7 +64,10 @@ function Form1({ handleModal, showModal }) {
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this tex t in a modal!</Modal.Body>
+        <Modal.Body>
+        <Camera setParentState={setStateFromChild}/>
+          <div>{sneakersDetails.name}</div>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleModal}>
             Close
